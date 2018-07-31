e249df68f6cd029b2b11c71bcda6394e98cd813a
CatcherInSky <zhangchunxiang98@Gmail.com>
2018-07-31 20:20:17 +0800

    ChemUni

--- 20180730GreenHopeDisplay/pages/intro/intro.js
+++ 20180730GreenHopeDisplay/pages/intro/intro.js
@@ -5,7 +5,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    advantage:[{
+      img:"../../images/intro/qual.jpg",
+      sec:"执着于品质追求",
+      con:"创始至今，我们的业务方向始终不变，专注造就品质，追求成就高端，在未来我们也将基于此信念继续前行。"
+    },{
+      img:"../../images/intro/proce.jpg",
+      sec:"严谨的服务流程",
+      con: "在不断创新和实践中总结出可持续的服务流程并坚持与客户一起思考，实现客户产品和企业品牌价值的提升。"
+    },{
+      img:"../../images/intro/exp.jpg",
+      sec:'丰富的项目经验',
+      con:"青朴积累了大量优秀的项目案例，在不断探索中总结经验，以结果为导向，持续为更多客户提供优质的服务。"
+    },{
+      img:"../../images/intro/serv.jpg",
+      sec:"完善的售后服务",
+      con:"所有项目承诺提供一年的技术维护服务，同时提供永久免费的技术咨询服务，一次合作，成就终身朋友。"
+    }],
+    ser:[{
+      img:'../../images/intro/cop.jpg',
+      sec:"零风险合作",
+      con:"侧重以结果为服务导向"
+    }, {
+        img: '../../images/intro/need.jpg',
+        sec: "以需求为核心",
+        con: "以需求为核心的发展方向"
+      }, {
+        img: '../../images/intro/int.jpg',
+        sec: "交互动效",
+        con: "精致有趣的交互动效"
+      }, {
+        img: '../../images/intro/fun.jpg',
+        sec: "功能开发",
+        con: "资深工程师为您开发项目"
+      }, {
+        img: '../../images/intro/test.jpg',
+        sec: "测试服务",
+        con: "严格把关产品质量"
+      }, {
+        img: '../../images/intro/ser.jpg',
+        sec: "售后服务",
+        con: "永久免费的基础服务支持"
+      }]
   },
 
   /**
@@ -15,6 +56,7 @@ Page({
   
   },
 
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
