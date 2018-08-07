08355d4d22df6fdc710c996fc1ab8090ccce2d46
CatcherInSky <zhangchunxiang98@Gmail.com>
2018-08-07 20:14:06 +0800

    All column

--- 20180731ChemUni/src/JS/main.js
+++ 20180731ChemUni/src/JS/main.js
@@ -1,26 +1,14 @@
-var mySwiper = new Swiper('.pc-slider .swiper-container',{
-    loop: true,
-    autoplay: 3000,
-    calculateHeight:true,
-    pagination:'.pc-slider .pagination',
-    paginationClickable: true,
-
-});
-var Swiper = new Swiper('.mobile-slider .swiper-container',{
-    loop: true,
-    autoplay: 3000,
-    calculateHeight:true,
-    pagination:'.mobile-slider .pagination',
-    paginationClickable: true,
-
-});
-function closeNav(){
-    var div = document.getElementById("nav");
-    div.style.display = "none";
-}
-
 function dropDown(){
     var div = document.getElementById("nav");
-    console.log(div);
     div.style.display = "block";
 }
+function closeNav(){
+    var div = document.getElementById("nav");
+    div.style.animation="navSlideOut 0.5s forwards";
+    setTimeout(Non,500);
+    function Non(){
+        div.style.display="none";
+        div.style.animation="navSlide 0.5s";
+    }
+}
+
